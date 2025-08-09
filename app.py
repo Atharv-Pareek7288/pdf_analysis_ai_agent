9c079798a5c4263189b2d64a064d80c193188c27
Atharv-Pareek7288 <atharvpareek.jod@gmail.com>
2025-08-09 10:00:04 +0530

    integrated the gemini LLM using the api key and gave it a simple prompt

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
+import os
+from dotenv import load_dotenv
+import google.generativeai as genai
 import streamlit as st
 import fitz
+
+
+load_dotenv()
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+model = genai.GenerativeModel("gemini-1.5-flash-002")
+response = model.generate_content("Explain why ai will take over humans in near future in one line.")
+print (response.text)
 
 st.title("PDF Analysing Agent")
 uploaded_file = st.file_uploader("Upload your PDF", type="pdf")
