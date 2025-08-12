5000bd190aa97701c65bf134a5b82d939df4c67d
Atharv-Pareek7288 <atharvpareek.jod@gmail.com>
2025-08-12 23:11:41 +0530

    add the gemini to summarize the pdf content and provide a detailed explaination and ask 5 follow-up questions to better explain the topic in pdf

--- app.py
+++ app.py
@@ -1,27 +1,32 @@
-import os
-from dotenv import load_dotenv
-import google.generativeai as genai
 import streamlit as st
+import os
 import fitz
-
+import google.generativeai as genai
+from dotenv import load_dotenv
 
 load_dotenv()
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel("gemini-1.5-flash-002")
-response = model.generate_content("Explain why ai will take over humans in near future in one line.")
-print (response.text)
+genai.configure(api_key=os.getenv("GEMINI_API_KEYS"))
+model  = genai.GenerativeModel("gemini-1.5-flash-002")
 
-st.title("PDF Analysing Agent")
-uploaded_file = st.file_uploader("Upload your PDF", type="pdf")
+st.title("Studify")
+st.text("your study buddy here to help you... just upload your study material and lets conquer the exams!")
+uploaded_file = st.file_uploader("Upload your PDF", type="PDF")
 
 text = ""
 
 if uploaded_file is not None:
-    with st.spinner("Reading your PDF..."):
-        doc = fitz.open(stream=uploaded_file.read(), filetype="pdf")
-        for page in doc:
-            text += page.get_text()
+    doc = fitz.open(stream=uploaded_file.read(), filetype="pdf")
+    for page in doc:
+        out = page.get_text()
+        text += out
 
-        st.success("PDF loaded successfully!")
-        st.write("Here's preview of the text: ")
-        st.text(text[:1000])
+prompt = f"""
+think of yourself as a great explainer about the topic which the user will give... summarize the given text in a detailed manner
+ask 5 follow up questions from the user about the topic of text he has uplaoded and the questions must be highly relevent, think of yourself as the teacher so that you undertand what topics are import for the exmas 
+and explain the in the best way possible 
+{text}
+"""
+
+response = model.generate_content(prompt)
+
+st.write(response.text)
