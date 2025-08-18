92558bacebfd3ab6a94b9aa4d8eebd0a4a79eda0
Atharv-Pareek7288 <atharvpareek.jod@gmail.com>
2025-08-18 18:37:02 +0530

    now the text from the pdf goes into gemini to get summarized and gets displayed

--- app.py
+++ app.py
@@ -3,33 +3,56 @@ import os
 import fitz
 from dotenv import load_dotenv
 from google import genai
+import time
 
 load_dotenv()
 api_key=os.getenv("GEMINI_API_KEYS")
 client = genai.Client(api_key=api_key)
-
+st.title("EchoLens")
 user_input = st.chat_input("Your Message", accept_file = True, file_type = ["pdf"])
 
 if user_input:
-    st.chat_message("user").write(user_input.text)
 
-    response = client.models.generate_content_stream(
-    model = "gemini-2.5-flash",
-    contents =user_input.text
-)
+    if user_input.files:
+        uploaded_file = user_input.files[0]
+        st.chat_message("user").write(f"Uploaded file 📄 {uploaded_file.name}")
 
-    with st.chat_message("assistant"):
-        result=""
-        for chunk in response:
-            if chunk.text:
-                result+=chunk.text
-        st.write(result)
-text = ""
-#added the pdf uploading system inside the chatbox... just like the chatgpt 
-if user_input and user_input.files:
-    uploaded_file = user_input.files[0]
-    dov = fitz.open(stream=uploaded_file.read(), filetype="pdf")
-    for page in dov:
-        out = page.get_text()
-        text += out
-st.chat_message("assistant").write(text)
+        text = ""
+        doc = fitz.open(stream=uploaded_file.read(), filetype="pdf")
+        for page in doc:
+            out = page.get_text()
+            text += out
+
+        response = client.models.generate_content_stream(
+            model= "gemini-2.5-flash",
+            contents = f"summarize the give text in such a way that it covers all the important topics ... {text}"
+
+        )
+
+        with st.chat_message("assistant"):
+            placeholder=st.empty()
+            result=""
+            for chunk in response:
+                if chunk.text:
+                    result+=chunk.text
+                    placeholder.markdown(result)
+                    time.sleep(0.02)
+            
+    elif user_input.text:
+        st.chat_message("user").write(user_input.text)
+
+        response = client.models.generate_content_stream(
+        model = "gemini-2.5-flash",
+        contents =[user_input.text]
+    )
+
+        with st.chat_message("assistant"):
+            placeholder=st.empty()
+            result=""
+            for chunk in response:
+                if chunk.text:
+                    result+=chunk.text
+                    placeholder.markdown(result)
+                    time.sleep(0.02)
+
+
